f39818267bbc42a856f33c7a7fa22c31719ae173
Christian Beier <109050697+cbeier-studio@users.noreply.github.com>
2025-01-29 07:59:46 -0300

    Exploratory attempt to add a trendline to species accumulation chart, not implemented

--- lib/screens/inventory/species_chart_screen.dart
+++ lib/screens/inventory/species_chart_screen.dart
@@ -1,42 +1,79 @@
-import 'dart:math';
+// import 'package:equations/equations.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:xolmis/generated/l10n.dart';
 
 import '../../data/models/inventory.dart';
 
-class SpeciesChartScreen extends StatelessWidget {
+class SpeciesChartScreen extends StatefulWidget {
   final Inventory inventory;
 
   const SpeciesChartScreen({super.key, required this.inventory});
 
   @override
+  State<SpeciesChartScreen> createState() => _SpeciesChartScreenState();
+}
+
+class _SpeciesChartScreenState extends State<SpeciesChartScreen> {
+  late List<SpeciesAccumulationData> speciesAccumulationData;
+  List<FlSpot> trendLineSpots = [];
+  late double minX;
+  late double maxX;
+  late double maxY;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeChartData();
+  }
+
+  // Initialize the chart data
+  void _initializeChartData() {
+    speciesAccumulationData = _prepareSpeciesAccumulationData(widget.inventory);
+    _calculateChartBounds();
+    // _calculateTrendLine();
+  }
+
+  // Set the chart bounds
+  void _calculateChartBounds() {
+    minX = 0.0;
+    maxX = speciesAccumulationData.isNotEmpty
+        ? speciesAccumulationData
+        .map((data) => data.interval.toDouble())
+        .reduce((a, b) => a > b ? a : b)
+        : 0.0;
+    maxY = speciesAccumulationData.isNotEmpty
+        ? speciesAccumulationData
+        .map((data) => data.speciesCount.toDouble())
+        .reduce((a, b) => a > b ? a : b)
+        : 0.0;
+  }
+
+  // EXPERIMENTAL
+  // Prepare the data for the trend line
+  // void _calculateTrendLine() {
+  //   if (speciesAccumulationData.isEmpty) {
+  //     trendLineSpots = [];
+  //     return;
+  //   }
+  //
+  //   final List<InterpolationNode> nodes = speciesAccumulationData
+  //       .map((point) => InterpolationNode(
+  //       x: point.interval.toDouble(), y: point.speciesCount.toDouble()))
+  //       .toList();
+  //
+  //   final splineInterpolation = PolynomialInterpolation(nodes: nodes);
+  //
+  //   trendLineSpots = [];
+  //   final step = (nodes.last.x - nodes.first.x) / 1000;
+  //   for (double x = nodes.first.x; x <= nodes.last.x; x += step) {
+  //     final y = splineInterpolation.compute(x);
+  //     trendLineSpots.add(FlSpot(x, y));
+  //   }
+  // }
+
+  @override
   Widget build(BuildContext context) {
-    // Prepare the data for the species accumulation curve
-    final speciesAccumulationData = _prepareSpeciesAccumulationData(inventory);
-    List<FlSpot> trendLineSpots = [];
-
-    // Set the chart bounds
-    final minX = 0.0;
-    final maxX = speciesAccumulationData.isNotEmpty
-        ? speciesAccumulationData.map((data) => data.interval.toDouble()).reduce((a, b) => a > b ? a : b)
-        : 0.0;
-    final maxY = speciesAccumulationData.isNotEmpty
-        ? speciesAccumulationData.map((data) => data.speciesCount.toDouble()).reduce((a, b) => a > b ? a : b)
-        : 0.0;
-
-    // Prepare the data for the trend line using polyfit
-    if (speciesAccumulationData.isNotEmpty) {
-      final x = speciesAccumulationData.map((data) => data.interval.toDouble()).toList();
-      final y = speciesAccumulationData.map((data) => data.speciesCount.toDouble()).toList();
-      final polyCoefficients = _polyfit(x, y, 2); // Fit a 2nd degree polynomial
-
-      for (var i = minX; i <= maxX; i++) {
-        final yValue = _polyval(polyCoefficients, i);
-        trendLineSpots.add(FlSpot(i, yValue));
-      }
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text(S.of(context).speciesAccumulationCurve),        
@@ -84,6 +121,7 @@ class SpeciesChartScreen extends StatelessWidget {
               border: Border.all(color: Colors.grey, width: 1),
             ),
             lineBarsData: [
+              // Show the line of accumulated species
               LineChartBarData(
                 spots: speciesAccumulationData
                     .map((data) => FlSpot(data.interval.toDouble(), data.speciesCount.toDouble()))
@@ -98,15 +136,16 @@ class SpeciesChartScreen extends StatelessWidget {
                   color: Colors.deepPurpleAccent.withAlpha(30),
                 ),
               ),
-              LineChartBarData(
-                show: false,
-                    spots: trendLineSpots,
-                    isCurved: false,
-                    color: Colors.red,
-                    barWidth: 2,
-                    isStrokeCapRound: true,
-                    dotData: FlDotData(show: false),
-                  ),
+              // Show the trend/smoothed line
+              // LineChartBarData(
+              //   show: true,
+              //       spots: trendLineSpots,
+              //       isCurved: false,
+              //       color: Colors.red,
+              //       barWidth: 2,
+              //       isStrokeCapRound: true,
+              //       dotData: FlDotData(show: false),
+              //     ),
             ],
           ),
         ),
@@ -162,95 +201,6 @@ class SpeciesChartScreen extends StatelessWidget {
     }
 
     return speciesAccumulationData;
-  }
-
-  List<double> _polyfit(List<double> x, List<double> y, int degree) {
-    final n = x.length;
-    final X = List.generate(n, (i) => List.generate(degree + 1, (j) => pow(x[i], j).toDouble()));
-    final XT = _transpose(X);
-    final XTX = _multiplyMatrices(XT, X);
-    final XTY = _multiplyMatrixVector(XT, y);
-    final coefficients = _solve(XTX, XTY);
-    return coefficients;
-  }
-
-  double _polyval(List<double> coefficients, double x) {
-    return coefficients.asMap().entries.fold(0.0, (sum, entry) => sum + entry.value * pow(x, entry.key));
-  }
-
-  List<List<double>> _transpose(List<List<double>> matrix) {
-    final rows = matrix.length;
-    final cols = matrix[0].length;
-    final transposed = List.generate(cols, (i) => List.generate(rows, (j) => matrix[j][i]));
-    return transposed;
-  }
-
-  List<List<double>> _multiplyMatrices(List<List<double>> A, List<List<double>> B) {
-    final rowsA = A.length;
-    final colsA = A[0].length;
-    final colsB = B[0].length;
-    final result = List.generate(rowsA, (i) => List.generate(colsB, (j) => 0.0));
-    for (var i = 0; i < rowsA; i++) {
-      for (var j = 0; j < colsB; j++) {
-        for (var k = 0; k < colsA; k++) {
-          result[i][j] += A[i][k] * B[k][j];
-        }
-      }
-    }
-    return result;
-  }
-
-  List<double> _multiplyMatrixVector(List<List<double>> matrix, List<double> vector) {
-    final rows = matrix.length;
-    final cols = matrix[0].length;
-    final result = List.generate(rows, (i) => 0.0);
-    for (var i = 0; i < rows; i++) {
-      for (var j = 0; j < cols; j++) {
-        result[i] += matrix[i][j] * vector[j];
-      }
-    }
-    return result;
-  }
-
-  List<double> _solve(List<List<double>> A, List<double> b) {
-    final n = A.length;
-    final x = List.generate(n, (i) => 0.0);
-    for (var i = 0; i < n; i++) {
-      var maxEl = A[i][i];
-      var maxRow = i;
-      for (var k = i + 1; k < n; k++) {
-        if (A[k][i] > maxEl) {
-          maxEl = A[k][i];
-          maxRow = k;
-        }
-      }
-      for (var k = i; k < n; k++) {
-        final tmp = A[maxRow][k];
-        A[maxRow][k] = A[i][k];
-        A[i][k] = tmp;
-      }
-      final tmp = b[maxRow];
-      b[maxRow] = b[i];
-      b[i] = tmp;
-      for (var k = i + 1; k < n; k++) {
-        final c = -A[k][i] / A[i][i];
-        for (var j = i; j < n; j++) {
-          if (i == j) {
-            A[k][j] = 0;
-          } else {
-            A[k][j] += c * A[i][j];
-          }
-        }
-        b[k] += c * b[i];
-      }
-    }
-    for (var i = n - 1; i >= 0; i--) {
-      x[i] = b[i] / A[i][i];
-      for (var k = i - 1; k >= 0; k--) {
-        b[k] -= A[k][i] * x[i];
-      }
-    }
-    return x;
   }
 }
 
